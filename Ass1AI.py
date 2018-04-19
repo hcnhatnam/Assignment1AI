6377f511df30e9ed66bbeb636be94edd77920004
hcnhatnam <37246426+hcnhatnam@users.noreply.github.com>
2018-04-19 18:23:08 +0700

    Add files via upload

--- Ass1AI.py
+++ Ass1AI.py
@@ -1,10 +1,12 @@
+import bloxorz as blo
+import time
 class State:
     def __init__(self,x1,y1,x2,y2,oriented,parent):
         self.x1=x1
         self.y1=y1
         self.x2=x2      #x2<0 cot dung
         self.y2=y2
-        self.oriented=oriented  #0:Vertical,1:HorizontalX;2:HorizontalY;
+        self.oriented=oriented  #0:Vertical,1:song song truc x(|); 2:song song Y(--);
         self.parent=parent
 
 class Bloxorz:
@@ -25,62 +27,70 @@ class Bloxorz:
                 stack+=self.successor(currentState,listVisted)
             listVisted.append(currentState)
         return None
+
+
     def isGoal(self,currentState):
         return self.goalState.x1==currentState.x1 and self.goalState.y1==currentState.y1 and currentState.oriented==0
+
+
     def isVisted(self,currentState,listVisted):
         for i in listVisted:
             if i.x1==currentState.x1 and i.y1==currentState.y1 and  i.x2==currentState.x2 and i.y2==currentState.y2 and i.oriented==currentState.oriented:
                 return True
         return False
+
+
     def successor(self,currentState:State,listVisted):
         listSuccessor=[]
         if  currentState.oriented==0:# Column is Vertical
-            stateRight=State(currentState.x1+1,currentState.y1,currentState.x1+2,currentState.y1,1,currentState)
+            stateRight=State(currentState.x1,currentState.y1+1,currentState.x1,currentState.y1+2,2,currentState)
             if(self.isValidState(stateRight) and not self.isVisted(stateRight,listVisted)):
                 listSuccessor.append(stateRight)
 
-            stateLeft=State(currentState.x1-2,currentState.y1,currentState.x1-1,currentState.y1,1,currentState)
+            stateLeft=State(currentState.x1,currentState.y1-2,currentState.x1,currentState.y1-1,2,currentState)
             if(self.isValidState(stateLeft)and not self.isVisted(stateLeft,listVisted)):
                 listSuccessor.append(stateLeft)
 
-            stateTop=State(currentState.x1,currentState.y1-2,currentState.x1,currentState.y1-1,2,currentState)#mapMatrix có hàng 1 cột 1=0
+            stateTop=State(currentState.x1-2,currentState.y1,currentState.x1-1,currentState.y1,1,currentState)#mapMatrix có hàng 1 cột 1=0
             if(self.isValidState(stateTop)and not self.isVisted(stateTop,listVisted)):
                 listSuccessor.append(stateTop)
 
-            stateDown=State(currentState.x1,currentState.y1+1,currentState.x1,currentState.y1+2,2,currentState)#mapMatrix có hàng 1 cột 1=0
+            stateDown=State(currentState.x1+1,currentState.y1,currentState.x1+2,currentState.y1,1,currentState)#mapMatrix có hàng 1 cột 1=0
             if(self.isValidState(stateDown)and not self.isVisted(stateDown,listVisted)):
                 listSuccessor.append(stateDown)
         else:
-            if currentState.oriented==1: #Column is HorizontalY
-                stateRight=State(currentState.x2+1,currentState.y1,-1,-1,0,currentState)
+            if currentState.oriented==2: #2:song song Y(--);
+                stateRight=State(currentState.x2,currentState.y1+1,-1,-1,0,currentState)
                 if(self.isValidState(stateRight)and not self.isVisted(stateRight,listVisted)):
                     listSuccessor.append(stateRight)
 
-                stateLeft=State(currentState.x1-1,currentState.y1,-1,-1,0,currentState)
+                stateLeft=State(currentState.x1,currentState.y1-1,-1,-1,0,currentState)
                 if(self.isValidState(stateLeft)and not self.isVisted(stateLeft,listVisted)):
                     listSuccessor.append(stateLeft)
 
-                stateTop=State(currentState.x1,currentState.y1-1,currentState.x2,currentState.y2-1,1,currentState)#mapMatrix có hàng 1 cột 1=0
+                stateTop=State(currentState.x1-1,currentState.y1,currentState.x2-1,currentState.y2,2,currentState)#mapMatrix có hàng 1 cột 1=0
                 if(self.isValidState(stateTop)and not self.isVisted(stateTop,listVisted)):
                     listSuccessor.append(stateTop)
 
-                stateDown=State(currentState.x1,currentState.y1+1,currentState.x2,currentState.y2+1,1,currentState)#mapMatrix có hàng 1 cột 1=0
+                stateDown=State(currentState.x1+1,currentState.y1,currentState.x2+1,currentState.y2,2,currentState)#mapMatrix có hàng 1 cột 1=0
                 if(self.isValidState(stateDown)and not self.isVisted(stateDown,listVisted)):
                     listSuccessor.append(stateDown)
-            if currentState.oriented==2: #Column is HorizontalX
-                stateRight=State(currentState.x1+1,currentState.y1,currentState.x2+1,currentState.y2,2,currentState)
+
+
+            if currentState.oriented==1: #1:song song truc x(|)
+                stateRight=State(currentState.x1,currentState.y1+1,currentState.x2,currentState.y2+1,1,currentState)
                 if(self.isValidState(stateRight)and not self.isVisted(stateRight,listVisted)):
                     listSuccessor.append(stateRight)
 
-                stateLeft=State(currentState.x1-1,currentState.y1,currentState.x2-1,currentState.y2,2,currentState)
+                stateLeft=State(currentState.x1,currentState.y1-1,currentState.x2,currentState.y2-1,1,currentState)
                 if(self.isValidState(stateLeft)and not self.isVisted(stateLeft,listVisted)):
                     listSuccessor.append(stateLeft)
 
-                stateTop=State(currentState.x1,currentState.y1-1,-1,-1,0,currentState)#mapMatrix có hàng 1 cột 1=0
+                stateTop=State(currentState.x1-1,currentState.y1,-1,-1,0,currentState)#mapMatrix có hàng 1 cột 1=0
                 if(self.isValidState(stateTop)and not self.isVisted(stateTop,listVisted)):
                     listSuccessor.append(stateTop)
 
-                stateDown=State(currentState.x2,currentState.y2+1,-1,-1,0,currentState)#mapMatrix có hàng 1 cột 1=0
+                stateDown=State(currentState.x2+1,currentState.y2,-1,-1,0,currentState)#mapMatrix có hàng 1 cột 1=0
                 if(self.isValidState(stateDown)and not self.isVisted(stateDown,listVisted)):
                     listSuccessor.append(stateDown)
         return listSuccessor
@@ -96,19 +106,47 @@ class Bloxorz:
         return False
 
 def printResult(lastState:State):
+    listResult=[]
     path=lastState
     while path!=None:
-        print("("+str(path.x1)+","+str(path.y1)+")"+"|"+"("+str(path.x2)+","+str(path.y2)+")")
+        listResult.append(path)
         path=path.parent
+
+    while listResult.__len__()!=0:
+        time.sleep(0.3)
+        top=listResult.pop()
+        print("("+str(top.x1)+","+str(top.y1)+")"+"|"+"("+str(top.x2)+","+str(top.y2)+")"+"|"+str(top.oriented))
+        blo.drawBlo(top.x1,top.y1,top.oriented)
+
+
+
+
 def main():
-    matrixMap=[[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],
-               [0,0,1,1,1,0,0,0,0,0],[0,0,1,1,1,1,0,0,0,0],
-               [0,0,1,1,1,1,0,0,0,0],[0,0,0,1,1,1,0,0,0,0],
-               [0,0,0,1,1,1,0,0,0,0],[0,0,0,1,1,1,1,0,0,0],
-               [0,0,0,0,1,1,1,1,0,0],[0,0,0,0,1,1,-1,1,0,0],
-               [0,0,0,0,1,1,1,1,0,0],[0,0,0,0,0,1,1,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
-    bloxorz=Bloxorz(matrixMap,State(3,3,-1,-1,0,None),State(9,6,-1,-1,0,None))
+    matrixMap=[[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,1,1,1,0,0,0,0,0,0,0,0,0],
+               [0,0,1,1,1,1,1,1,0,0,0,0,0,0],
+               [0,0,1,1,1,1,1,1,1,1,1,0,0,0],
+               [0,0,0,1,1,1,1,1,1,1,1,1,0,0],
+               [0,0,0,0,0,0,0,1,1,-1,1,1,0,0],
+               [0,0,0,0,0,0,0,0,1,1,1,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+    bloxorz=Bloxorz(matrixMap,State(3,3,-1,-1,0,None),State(6,9,-1,-1,0,None))
     printResult(bloxorz.SolveDFS())
+
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
--- bloxorz.py
+++ bloxorz.py
@@ -1,9 +1,8 @@
 import pygame, math
-from pygame import gfxdraw
 
-pygame.init()
-screen_width = 700
-screen_height = 400
+
+screen_width = 1000
+screen_height = 700
 startX = 100
 startY = 200
 length = 25
@@ -12,14 +11,13 @@ ang = 15
 
 display = pygame.display.set_mode((screen_width,screen_height))
 
-
 class Block:
     def __init__(self):
         self.x = 100
         self.y = 200
         self.width = 40
         self.height = 30
-        
+
     def drawVertical(self):
         x,y,w,h = self.x,self.y,self.width,self.height
         b_w = 80
@@ -31,28 +29,30 @@ class Block:
         pygame.draw.line(display,(0,255,0),(x+ang+w,y-10+h),(x+ang+w,y-10+h-b_w)) #Cot truoc phai
         pygame.draw.line(display,(255,0,0),(x+ang,y+h),(x+ang,y+h-b_w)) #Cot truoc trai
 
-        b_l = (x+ang,y+h)
-        t_l = (x+ang,y+h-b_w)
-        b_r = (x+ang+w,y-10+h)
-        t_r = (x+ang+w,y-10+h-b_w)
-        t_l_r = self.rotate_around(90,b_l,t_l)
-        pygame.draw.circle(display,(255,255,255),t_l_r,10)
 
-    def drawHorizontal(self):
+    def drawHorizontalX(self):
         x,y,w,h = self.x,self.y,self.width,self.height
-        b_w = 80
-        pygame.draw.polygon(display,(255,0,0),[(x-10+w,y-10),(x+ang+w,y-10+h),(x+ang+w,y-10+h-b_w),(x-10+w,y-10-b_w)])#Mat phai
-        pygame.draw.polygon(display,(0,255,0),[(x-10+w,y-10),(x-10,y),(x-10,y-b_w),(x-10+w,y-10-b_w)]) #Mat sau
+        b_w = 40
+        deltax=40
+        deltay=10
+        pygame.draw.polygon(display,(255,0,0),[(x+deltax-10+w,y-deltay-10),(x+deltax+ang+w,y-deltay-10+h),(x+deltax+ang+w,y-deltay-10+h-b_w),(x+deltax-10+w,y-deltay-10-b_w)])#Mat phai
+        pygame.draw.polygon(display,(0,255,0),[(x+deltax-10+w,y-deltay-10),(x-10,y),(x-10,y-b_w),(x+deltax-10+w,y-10-b_w-deltay)]) #Mat sau
         pygame.draw.polygon(display,(0,0,255),[(x-10,y),(x+ang,y+h),(x+ang,y+h-b_w),(x-10,y-b_w)]) #Mat trai
-        pygame.draw.polygon(display,(255,255,255),[(x+ang+w,y-10+h),(x+ang,y+h),(x+ang,y+h-b_w),(x+ang+w,y-10+h-b_w)])  #Mat truoc
-        pygame.draw.line(display,(0,255,0),(x+ang+w,y-10+h),(x+ang+w,y-10+h-b_w)) #Cot truoc phai
+        pygame.draw.polygon(display,(255,255,255),[(x+deltax+ang+w,y-deltay-10+h),(x+ang,y+h),(x+ang,y+h-b_w),(x+deltax+ang+w,y-10+h-b_w-deltay)])  #Mat truoc
+        pygame.draw.line(display,(0,255,0),(x+deltax+ang+w,y-deltay-10+h),(x+deltax+ang+w,y-deltay-10+h-b_w)) #Cot truoc phai
         pygame.draw.line(display,(255,0,0),(x+ang,y+h),(x+ang,y+h-b_w)) #Cot truoc trai
-        b_l = (x+ang,y+h)
-        t_l = (x+ang,y+h-b_w)
-        b_r = (x+ang+w,y-10+h)
-        t_r = (x+ang+w,y-10+h-b_w)
-        t_l_r = self.rotate_around(90,b_l,t_l)
-        pygame.draw.circle(display,(255,255,255),t_l_r,10)
+
+    def drawHorizontalY(self):
+        x,y,w,h = self.x,self.y,self.width,self.height
+        b_w = 40
+        deltaX = 25
+        deltaY = 30
+        pygame.draw.polygon(display,(255,0,0),[(x-10+w,y-10),(x+ang+w+deltaX,y-10+h+deltaY),(x+ang+w+deltaX,y-10+h-b_w+deltaY),(x-10+w,y-10-b_w)])#Mat phai
+        pygame.draw.polygon(display,(0,255,0),[(x-10+w,y-10),(x-10,y),(x-10,y-b_w),(x-10+w,y-10-b_w)]) #Mat sau
+        pygame.draw.polygon(display,(0,0,255),[(x-10,y),(x+ang+deltaX,y+h+deltaY),(x+ang+deltaX,y+h-b_w+deltaY),(x-10,y-b_w)]) #Mat trai
+        pygame.draw.polygon(display,(255,255,255),[(x+ang+w+deltaX,y-10+h+deltaY),(x+ang+deltaX,y+h+deltaY),(x+ang+deltaX,y+h-b_w+deltaY),(x+ang+w+deltaX,y-10+h-b_w+deltaY)])  #Mat truoc
+        #pygame.draw.line(display,(0,255,0),(x+ang+w-deltaX,y-10+h-deltaY),(x+ang+w-deltaX,y-10+h-b_w-deltaY)) #Cot truoc phai
+        pygame.draw.line(display,(255,0,0),(x+ang+deltaX,y+h+deltaY),(x+ang+deltaX,y+h-b_w+deltaY)) #Cot truoc trai
 
     def rotate_around(self,degrees,centre,point):
         x,y = point
@@ -95,7 +95,7 @@ def draw_level(level_data):
         x=startX+(i*length)
         y=startY+(i*width)
         i+=1
-
+state=0
 def handle_events():
     for e in pygame.event.get():
         if(e.type == pygame.KEYDOWN):
@@ -109,44 +109,43 @@ def handle_events():
             if(e.key == pygame.K_RIGHT):
                 bloxor.x+=40
                 bloxor.y-=10
+                state=1
                 print("RIGHT")
             if(e.key == pygame.K_UP):
+                bloxor.x+=40
+                bloxor.y-=10
                 print("UP")
             if(e.key == pygame.K_DOWN):
+
                 print("DOWN")
         if(e.type == pygame.QUIT):
             pygame.quit()
             quit()
 
 bloxor = Block()
-def main():
-
-    pygame.display.set_caption("Bloxorz")
-    #clock = pygame.time.Clock()
-
-    #bloxor.x = 165
-    #bloxor.y = 220
-
-    #level_array = [[1]]
-
-    level_array = [
-        [1,1,1,0,0,0,0,0,0,0],
-        [1,1,1,1,1,0,0,0,0,0],
-        [1,1,1,1,1,1,1,1,1,0],
-        [0,1,1,1,1,1,1,1,1,1],
-        [0,0,0,0,0,1,1,0,1,1],
-        [0,0,0,0,0,0,1,1,1,0],
-    ]
-    while(True):
-        #clock.tick(30)
-        handle_events()
-        display.fill((193,39,1)) #fill screen white
-
-        draw_level(level_array)
+pygame.init()
+pygame.display.set_caption("Bloxorz")
+level_array = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,1,1,1,0,0,0,0,0,0,0,0,0],
+               [0,0,1,1,1,1,1,1,0,0,0,0,0,0],
+               [0,0,1,1,1,1,1,1,1,1,1,0,0,0],
+               [0,0,0,1,1,1,1,1,1,1,1,1,0,0],
+               [0,0,0,0,0,0,0,1,1,-1,1,1,0,0],
+               [0,0,0,0,0,0,0,0,1,1,1,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+def drawBlo(x,y,oriented):
+    #clock.tick(30)
+    display.fill((193,39,1)) #fill screen white
+    bloxor.x=100+y*40+25*x
+    bloxor.y=200-y*10+30*x
+    draw_level(level_array)
+    if oriented==0:
         bloxor.drawVertical()
+    elif oriented==1:
+        bloxor.drawHorizontalY()
+    else:
+        bloxor.drawHorizontalX()
 
-        pygame.display.flip() #update screen
-
-
-if __name__ == "__main__":
-    main()
+    pygame.display.flip() #update screen
