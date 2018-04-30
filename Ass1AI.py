a6c2d728d898c2f861d80a4ce254f29c7f679f12
hcnhatnam <nhatnam10a3@gmail.com>
2018-04-30 18:04:57 +0700

    Update

--- Ass1AI.py
+++ Ass1AI.py
@@ -83,6 +83,8 @@ class Bloxorz:
             #time.sleep(0.1)
             #blo.level_array=currentState.matrixMap
             #blo.drawBlo(currentState.x1,currentState.y1,currentState.oriented,currentState.x2,currentState.y2)
+            if currentState.x1==3 and currentState.y1==13 and currentState.x2==3 and currentState.y2==14:
+                a=0
             if self.isGoal(currentState):
                 printResult(currentState)
                 sys.exit()
@@ -181,16 +183,66 @@ class Bloxorz:
             return  []
 
     def enterSpecialSquare(self,stateCheck:State):
+        specialSquare=[]
+        stateCheck.matrixMap=copy.deepcopy(stateCheck.matrixMap)#Copy matrixMap
+        if stateCheck.oriented==0:
+            if stateCheck.matrixMap[stateCheck.x1][stateCheck.y1]==2 or stateCheck.matrixMap[stateCheck.x1][stateCheck.y1]==3:#Đứng ở ô dấu X
+                for x in self.specialSquareList:
+                    if (x.xLocate==stateCheck.x1 and x.yLocate==stateCheck.y1 ):
+                        specialSquare.append(x)
+            elif stateCheck.matrixMap[stateCheck.x1][stateCheck.y1]==4:#Đứng ở ô chi đôi
+                for x in self.specialSquareList:
+                    if x.xLocate==stateCheck.x1 and x.yLocate==stateCheck.y1:
+                        stateCheck.x1,stateCheck.y1,stateCheck.x2,stateCheck.y2=x.Open[0][0],x.Open[0][1],x.Open[1][0],x.Open[1][1]
+                        stateCheck.oriented=3
+                        return stateCheck
+        elif stateCheck.oriented==1 or stateCheck.oriented==2:#Dang nằm ở ô đặc biệt(ô O rỗng)
+            if stateCheck.matrixMap[stateCheck.x1][stateCheck.y1]==3 and ((stateCheck.x1,stateCheck.y1) not in [(stateCheck.parent.x1,stateCheck.parent.y1),(stateCheck.parent.x2,stateCheck.parent.y2)]):
+                for x in self.specialSquareList:
+                    if (x.xLocate==stateCheck.x1 and x.yLocate==stateCheck.y1 ):
+                        specialSquare.append(x)
+            if stateCheck.matrixMap[stateCheck.x2][stateCheck.y2]==3 and((stateCheck.x2,stateCheck.y2) not in [(stateCheck.parent.x1,stateCheck.parent.y1),(stateCheck.parent.x2,stateCheck.parent.y2)]):
+                for x in self.specialSquareList:
+                    if (x.xLocate==stateCheck.x2 and x.yLocate==stateCheck.y2 ):
+                        specialSquare.append(x)
+        elif stateCheck.oriented==3:#2 khối tách đôi        #Chú ý: tránh trường hợp 1 khối nằm trên ô X 1 khối nằm trên ô O mà nó lại bật ô X lên
+            for x in list(filter(lambda x:x.attribute==3,self.specialSquareList)):#Lọc những ô O rỗnng
+                if  (x.xLocate==stateCheck.x1 and x.yLocate==stateCheck.y1) and (stateCheck.x1!=stateCheck.parent.x1 or stateCheck.y1!=stateCheck.parent.y1) :#Tránh trường hợp 1 khối nhở nằm ở 1 vị trí cố định và khối 2 di chuyền mà vẫn bật tắt=>Sai
+                    specialSquare.append(x)
+                elif (x.xLocate==stateCheck.x2 and x.yLocate==stateCheck.y2) and (stateCheck.x2!=stateCheck.parent.x2 or stateCheck.y2!=stateCheck.parent.y2):
+                    specialSquare.append(x)
+        #Xử lý luôn bật hoặc luôn tắt
+        for special in specialSquare:
+            if special.constance==0:
+                for i in special.Open:
+                    stateCheck.matrixMap[i[0]][i[1]] = int(not stateCheck.matrixMap[i[0]][i[1]])#toggle
+            elif special.constance==1:#Luon Bat
+                if  stateCheck.matrixMap[special.Open[0][0]][special.Open[0][1]] == 0:
+                    for i in special.Open:
+                        stateCheck.matrixMap[i[0]][i[1]] = 1
+            elif special.constance==2:#Luon Tat
+                if  stateCheck.matrixMap[special.Open[0][0]][special.Open[0][1]] == 1:
+                    for i in special.Open:
+                        stateCheck.matrixMap[i[0]][i[1]] = 0
+        return stateCheck
+        '''
         if (stateCheck.oriented==0 and stateCheck.matrixMap[stateCheck.x1][stateCheck.y1]==2) or (stateCheck.matrixMap[stateCheck.x1][stateCheck.y1]==3 or stateCheck.matrixMap[stateCheck.x2][stateCheck.y2]==3 ):# ô X
             specialSquare=[]
-            if(stateCheck.oriented!=3):
+            if(stateCheck.oriented!=3):#đang dính nhau (Đang đứng ở ô X hoặc O) hoặc đang nằm ở ô O
                 stateCheck.matrixMap=copy.deepcopy(stateCheck.matrixMap)#Copy matrixMap
-                for x in self.specialSquareList:
-                    if (x.xLocate==stateCheck.x1 and x.yLocate==stateCheck.y1) or (x.xLocate==stateCheck.x2 and x.yLocate==stateCheck.y2):
-                        specialSquare.append(x)
-            else:# tránh trường hợp 1 khối nằm trên ô X 1 khối nằm trên ô O
+                if(stateCheck.matrixMap[stateCheck.x1][stateCheck.y1]==2 or stateCheck.matrixMap[stateCheck.x1][stateCheck.y1]==3):#(x1,y1) la ô đặc biệt
+                    for x in self.specialSquareList:
+                        if (x.xLocate==stateCheck.x1 and x.yLocate==stateCheck.y1 and stateCheck.parent.x1!=stateCheck.x1 ):
+                            specialSquare.append(x)
+                if(stateCheck.matrixMap[stateCheck.x2][stateCheck.y2]==2 or stateCheck.matrixMap[stateCheck.x2][stateCheck.y2]==3):#(x2,y2) la ô đặc biệt
+                    for x in self.specialSquareList:
+                        if (x.xLocate==stateCheck.x2 and x.yLocate==stateCheck.y2):
+                            specialSquare.append(x)
+            else:# tránh trường hợp 1 khối nằm trên ô X 1 khối nằm trên ô O mà nó lại bật ô X lên
                 for x in list(filter(lambda x:x.attribute==3,self.specialSquareList)):
-                    if  (x.xLocate==stateCheck.x1 and x.yLocate==stateCheck.y1) or (x.xLocate==stateCheck.x2 and x.yLocate==stateCheck.y2):
+                    if  (x.xLocate==stateCheck.x1 and x.yLocate==stateCheck.y1) and (x.xLocate==stateCheck.parent.x1 and x.yLocate==stateCheck.parent.y1) :#Tránh trường hợp 1 khối nhở nằm ở 1 vị trí cố định và khối 2 di chuyền mà vẫn bật tắt=>Sai
+                        specialSquare.append(x)
+                    elif (x.xLocate==stateCheck.x2 and x.yLocate==stateCheck.y2) and (x.xLocate==stateCheck.parent.x2 and x.yLocate==stateCheck.parent.y2):
                         specialSquare.append(x)
                 stateCheck.matrixMap=copy.deepcopy(stateCheck.matrixMap)#Copy matrixMap
             for special in specialSquare:
@@ -214,6 +266,7 @@ class Bloxorz:
             stateCheck.x1,stateCheck.y1,stateCheck.x2,stateCheck.y2=specialSquare.Open[0][0],specialSquare.Open[0][1],specialSquare.Open[1][0],specialSquare.Open[1][1]
             stateCheck.oriented=3
         return stateCheck
+        '''
     def isValidState(self,stateCheck:State):
         if(stateCheck.oriented==0):
             if(stateCheck.matrixMap[stateCheck.x1][stateCheck.y1]==0 or stateCheck.matrixMap[stateCheck.x1][stateCheck.y1]==5):#Đang đứng 0 va go
@@ -236,13 +289,14 @@ def printResult(lastState:State):
         print("("+str(top.x1)+","+str(top.y1)+")"+"|"+"("+str(top.x2)+","+str(top.y2)+")"+"|"+str(top.oriented))
         blo.level_array=top.matrixMap
         blo.drawBlo(top.x1,top.y1,top.oriented,top.x2,top.y2)
-        time.sleep(0.2)
+        time.sleep(0.3)
 
 
 #Gỗ 5
 def main():
     mapMatrix = []
-    stage='Stage/Stage5.txt'
+
+    stage='Stage/Stage9.txt'
     with open(stage) as f:
         mapMatrix = [[int(x) for x in line.split(',')] for line in f]
     print(mapMatrix)
@@ -262,14 +316,21 @@ def main():
     elif stage=='Stage/Stage7.txt':
         bloxorz=Bloxorz(mapMatrix,[specialSquare(6,11,[(8,5)],2)],State(5,3,-1,-1,0,[],mapMatrix,None),State(5,15,-1,-1,0,[],mapMatrix,None))
     elif stage=='Stage/Stage8.txt':
-        bloxorz=Bloxorz(mapMatrix,[specialSquare(6,6,[(3,11),(9,11)],4)],State(6,3,-1,-1,0,[],mapMatrix,None),State(6,14,-1,-1,0,[],mapMatrix,None))
+        bloxorz=Bloxorz(mapMatrix,[specialSquare(6,6,[(3,11),(9,11)],4)],State(6,3,-1,-1,0,[],mapMatrix,None),State(6,15,-1,-1,0,[],mapMatrix,None))
     elif stage=='Stage/Stage9.txt':
-        bloxorz=Bloxorz(mapMatrix,[specialSquare(3,15,[(3,4),(4,14)],4)],State(3,3,-1,-1,0,[],mapMatrix,None),State(6,9,-1,-1,0,[],mapMatrix,None))
+        bloxorz=Bloxorz(mapMatrix,[specialSquare(3,15,[(3,4),(3,14)],4)],State(3,3,-1,-1,0,[],mapMatrix,None),State(5,9,-1,-1,0,[],mapMatrix,None))
     elif stage=='Stage/Stage10.txt':
         bloxorz=Bloxorz(mapMatrix,[specialSquare(3,14,[(3,11),(3,14)],4),specialSquare(11,7,[(3,5),(3,6)],3),specialSquare(11,13,[(3,8),(3,9),(4,14),(5,14)],2)],State(3,11,-1,-1,0,[],mapMatrix,None),State(3,3,-1,-1,0,[],mapMatrix,None))#Stage8
     elif stage=='Stage/Stage11.txt':
         bloxorz=Bloxorz(mapMatrix,[specialSquare(8,8,[(2,6),(3,6)],3,2)],State(7,2,-1,-1,0,[],mapMatrix,None),State(3,4,-1,-1,0,[],mapMatrix,None))
-
+    elif stage=='Stage/Stage12.txt':
+        bloxorz=Bloxorz(mapMatrix,[specialSquare(4,8,[(4,14)],2),specialSquare(2,14,[(6,8)],2)],State(8,4,-1,-1,0,[],mapMatrix,None),State(6,6,-1,-1,0,[],mapMatrix,None))
+    elif stage=='Stage/Stage13.txt':
+        bloxorz=Bloxorz(mapMatrix,[],State(5,14,-1,-1,0,[],mapMatrix,None),State(6,9,-1,-1,0,[],mapMatrix,None))
+    elif stage=='Stage/Stage14.txt':
+        bloxorz=Bloxorz(mapMatrix,[specialSquare(5,14,[(4,3),(4,4)],2),specialSquare(11,15,[(5,3),(5,4)],2)],State(4,6,-1,-1,0,[],mapMatrix,None),State(10,5,-1,-1,0,[],mapMatrix,None))
+    elif stage=='Stage/Stage15.txt':
+        bloxorz=Bloxorz(mapMatrix,[specialSquare(9,13,[(10,11),(10,12)],3,2),specialSquare(11,13,[(10,11),(10,12)],3,2),specialSquare(7,9,[(10,3),(3,15)],4),specialSquare(5,10,[(3,7),(3,8)],3),specialSquare(5,10,[(3,12),(3,13)],3),specialSquare(3,14,[(4,4),(4,5)],2),specialSquare(3,14,[(3,7),(3,8)],2)],State(10,3,-1,-1,0,[],mapMatrix,None),State(10,14,-1,-1,0,[],mapMatrix,None))
     blo.level_array=mapMatrix
     blo.drawBlo(bloxorz.startState.x1,bloxorz.startState.y1,bloxorz.startState.oriented)
     result=(bloxorz.SolveDFS())
